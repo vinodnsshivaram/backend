689c3e7b660dd2478b7c491c1ffdcb6884485e01
vinodnsshivaram <vinodnsshivaram@gmail.com>
2019-02-01 01:43:33 -0700

    added user endpoint with mongo.

---
+++ src/main/java/com/baagir/shopping/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.baagir.shopping.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    protected Object methodNotSupportedErrorHandler(HttpServletRequest request, Exception exception) throws Exception {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(request.getPathInfo());
+    }
+}
